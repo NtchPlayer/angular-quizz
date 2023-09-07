d8c01a26d60897abb3883ec4144b5b2a210d1edd
IcePlayer <nathanchevalet@orange.fr>
2023-09-07 12:20:18 +0200

    init file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CheckboxComponent } from './checkbox/checkbox.component';
+import { QuestionComponent } from './question/question.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CheckboxComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule,
